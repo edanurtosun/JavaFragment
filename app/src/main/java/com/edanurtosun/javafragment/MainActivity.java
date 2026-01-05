fd8505bae391e393488a6e22d3da14ea91cb59fb
edanurtosun <eedanurtosun@hotmail.com>
2026-01-05 22:02:36 +0300

    fragment completed

--- app/src/main/java/com/edanurtosun/javafragment/MainActivity.java
+++ app/src/main/java/com/edanurtosun/javafragment/MainActivity.java
@@ -8,8 +8,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +27,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goToFirst(View view){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
+        FirstFragment firstFragment = new FirstFragment();
+        //fragmentTransaction.add(R.id.frameLayout, firstFragment).commit();
+        fragmentTransaction.replace(R.id.frameLayout, firstFragment).commit();
     }
 
     public void goToSecond(View view){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
+        SecondFragment secondFragment = new SecondFragment();
+        fragmentTransaction.replace(R.id.frameLayout, secondFragment).commit();
     }
 }
